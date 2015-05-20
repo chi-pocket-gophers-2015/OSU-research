fc821550f897fd338643134a24b60c4de70a5950
JimJoyce <j.joyce92@gmail.com>
2015-05-20 11:02:06 -0500

    initializing repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OsuResearch::Application.config.secret_token = 'cc42a7538f310bfe11d54d7c4543ae069d1988fb6682d96c47259772d84c582f77d5a15267df586da0f30ac15df060b8c7960d62661b3472c548522b3dceff68'
