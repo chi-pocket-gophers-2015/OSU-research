a76948e30e1e33534b073d72583bd4bd2a12f6ba
petermanhart & srgupta <pair+petermanhart.srgupta@devbootcamp.com>
2015-05-20 18:09:15 -0500

    new user functionality fixed, can log in as staffer or faculty with 'secret code' form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,25 @@
 class User < ActiveRecord::Base
+  # attr_accessor :secret_code
   attr_protected
+
+  # has_secure_password
+
+  CODES = %w(faculty staffer)
 
   has_many :experiments, foreign_key: :staffer_id
   has_many :observations, through: :experiments
   has_many :proposals, foreign_key: :faculty_id
   has_many :comments
 
-  validates :username, :email, :faculty, presence: true
-  validates :password, presence: {message: "password can't be blank"},
+  validates :username, :email, presence: true
+  validates :faculty, inclusion: {in: [true, false],
+    message: "Invalid secret code"}
+  validates :password, presence: {message: "Password can't be blank"},
     confirmation: true
 
-  # def initialize(params)
-  #   super(params)
-  #   determine_faculty(params[:secret_code])
-  # end
-
   def determine_faculty(code)
-    self.faculty = true if code == 'faculty'
-    self.faculty = false if code == 'staffer'
+    self.faculty = true if code.downcase == 'faculty'
+    self.faculty = false if code.downcase == 'staffer'
   end
 
   def password
@@ -29,8 +31,8 @@ class User < ActiveRecord::Base
     self.password_hash = BCrypt::Password.create(pass)
   end
 
-  def valid_password(pass)
-    pass.length > 0
+  def secret_code=(code)
+    determine_faculty(code)
   end
 
   def self.authenticate(user_info)
