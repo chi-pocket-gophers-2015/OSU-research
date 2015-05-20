0d2436581183c66bcc248d8e53ac0a398d81712e
Jim Joyce <j.joyce92@gmail.com>
2015-05-20 18:19:21 -0500

    latest working copy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by_id(params[:id])
     render 'faculty_homepage' if @user && @user.faculty == true
-    render 'staffer_homepage' if @user && @user.faculty == false
+    render 'staffers_homepage' if @user && @user.faculty == false
   end
 
   def new
