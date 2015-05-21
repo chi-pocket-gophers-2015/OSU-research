634395ce19c45f684798ef2cddcc003ab20bf076
Peter Manhart <petermanhart@gmail.com>
2015-05-21 13:28:41 -0500

    fix proposal.active bug

--- db/migrate/20150520185836_create_proposals.rb
+++ db/migrate/20150520185836_create_proposals.rb
@@ -3,7 +3,7 @@ class CreateProposals < ActiveRecord::Migration
     create_table :proposals do |t|
       t.string :title
       t.string :hypothesis
-      t.boolean :active
+      t.boolean :active, default: true
       t.string :summary
       t.belongs_to :faculty, class_name: "User"
       t.belongs_to :category
