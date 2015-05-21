a8c8acd27dd9d076f2dfb08993877288f8a2063c
JimJoyce <j.joyce92@gmail.com>
2015-05-21 08:10:13 -0500

    logout and posting new proposals

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -4,6 +4,26 @@ class ProposalsController < ApplicationController
   end
 
   def new
+    @user = User.find_by_id(session[:user_id])
+    @proposal = @user.proposals.new
     render :new
+  end
+
+  def create
+    @user = User.find_by_id(session[:user_id])
+    @proposal = @user.proposals.new(proposal_params)
+    if @proposal.save
+      redirect_to @user
+    else
+      @errors = @proposal.errors.messages
+      render :new
+    end
+  end
+
+
+  private
+
+  def proposal_params
+    params.require(:proposal).permit(:title, :hypothesis, :summary, :status)
   end
 end
