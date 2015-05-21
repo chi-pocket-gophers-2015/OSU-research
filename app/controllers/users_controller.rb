36710c8fe104b2603f0c491bcd9e95755bacd3c6
Jim Joyce <j.joyce92@gmail.com>
2015-05-21 14:27:58 -0500

    added errors to homepage for faculty/staffers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
 
     @user = User.find_by_id(params[:id])
     session[:user_id] = @user.id
-    @proposals = @user.proposals.order('created_at desc')
+    @all_proposals = Proposal.all
+    @ordered_proposals = @user.proposals.order('created_at desc')
     render 'faculty_homepage' if @user && @user.faculty == true
     render 'staffers_homepage' if @user && @user.faculty == false
   end
