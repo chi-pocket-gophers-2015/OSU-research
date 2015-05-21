a7fcc3157f82a2c57e6de6bcc7d10931f2ae51ea
Peter Manhart <petermanhart@gmail.com>
2015-05-21 14:27:10 -0500

    adding protections to prohibit navigation to certain views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
   def show
-
-    @user = User.find_by_id(params[:id])
-    session[:user_id] = @user.id
+    redirect_to root_path if !current_user
+    @user = current_user
     @proposals = @user.proposals.order('created_at desc')
     render 'faculty_homepage' if @user && @user.faculty == true
     render 'staffers_homepage' if @user && @user.faculty == false
