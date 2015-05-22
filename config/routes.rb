89a91108aee2c56408a8dd1ee7dccae1aa73a4ae
Peter Manhart <petermanhart@gmail.com>
2015-05-22 11:07:56 -0500

    all observation creation and deletion functionality now working

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ OsuResearch::Application.routes.draw do
   resources :categories, only: [:show, :index]
   resources :users, except: [:edit, :update]
   resources :proposals, only: [:show, :new, :create]
-  resources :experiments, except: [:delete]
+  resources :experiments, except: [:destroy]
   # resources :observations, only: [:new, :show, :create, :destroy]
   resources :experiments do
-    resources :observations, only: [:new, :delete]
+    resources :observations, only: [:new, :create, :destroy]
   end
 
   root to: "sessions#new"
