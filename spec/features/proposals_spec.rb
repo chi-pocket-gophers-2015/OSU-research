0e8ec743205d4249acd9cd4028367032a0b8b9ce
Peter Manhart <petermanhart@gmail.com>
2015-05-22 15:23:57 -0500

    yet another partial commit

--- spec/features/proposals_spec.rb
+++ spec/features/proposals_spec.rb
@@ -4,16 +4,16 @@ RSpec.feature "Proposals", :type => :feature do
   let!(:fac) { User.create!(email: 'jjoyce1@me.com', username: 'staff', password: 'password', faculty: true) }
 
   scenario "click on proposal link" do
-  allow(User).to receive(:find_by_id).and_return(fac)
-  visit '/categories/1'
-
-
+    allow(User).to receive(:find_by_id).and_return(fac)
+    visit '/categories/1'
+    click_link(proposal_path(Proposal.first))
+    expect(current_path).to eq(proposal_path(Proposal.first))
   end
 
   scenario "click new proposal link"
-  allow(User).to receive(:find_by_id).and_return(fac)
+  # allow(User).to receive(:find_by_id).and_return(fac)
 
   scenario "create new proposal"
-  allow(User).to receive(:find_by_id).and_return(fac)
+  # allow(User).to receive(:find_by_id).and_return(fac)
 
 end
