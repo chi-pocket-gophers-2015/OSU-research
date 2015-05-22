54f786dae7d70854ade8f19663760c2badee617f
George Lee <ghrlee@gmail.com>
2015-05-22 14:57:20 -0500

    edited obs spec

--- spec/controllers/observation_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe ObservationsController, :type => :controller do
-
-end
---
+++ spec/controllers/observations_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe ObservationsController, :type => :controller do
+
+	describe "GET #new" do
+	end
+
+	describe "POST #destroy" do
+	end
+
+	describe "POST #create" do
+	end
+end
